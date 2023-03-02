460fe40067775b18921e0d781a6a6148ebd1d4e6
jerry840623 <179555310@qq.com>
2023-03-02 20:14:30 +0800

    clock

--- test2.py
+++ test2.py
@@ -341,6 +341,36 @@ if __name__ == "__main__":
     company_scale = my_company.check()
     my_company.show(company_scale)
 
+    win=Tk()
+win.geometry('400x400+200+200')
+win.title('Login')
+win.config(background='yellow')
+
+lb1=Label(win,text='UserName:',bg='green',fg='white').grid(row=0)
+lb2=Label(win,text='Password',bg='green',fg='white').grid(row=1)
+
+ent1=Entry(win,width=30).grid(row=0,column=1)
+ent2=Entry(win,show='********',width=30).grid(row=1,column=1)
+
+def LogSussess():
+    messagebox.showinfo('welcom','login sucsses')
+
+def LogCannel():
+    if messagebox.showwarning('Cannel','Sorry,Login Cannel'):
+        win.destroy()
+
+
+
+bt1=Button(win,text='Login',command=LogSussess).grid(row=2,column=0)
+bt2=Button(win,text='Cannel',command=LogCannel,).grid(row=2,column=1)
+
+ety=Entry(win)
+ety.grid(row=3,column=0)
+
+
+win.mainloop()
+
+
 
 """
 
