f8ad985e2e3d7eaa9a901185245a4246545fcb9d
jerry840623 <179555310@qq.com>
2023-03-06 19:54:54 +0800

    20230306-2

--- Day100/D6.py
+++ Day100/D6.py
@@ -13,8 +13,8 @@ print(count)
 
 
 import dd6_1
-'''
 
+# 练习1
 def gcd(x,y):
     if y>x:
         (x,y)=(y,x)
@@ -31,3 +31,46 @@ def lcm(x,y):
 print(lcm(8,6))
 
 
+
+# 练习2 实现判断一个数是不是回文数的函数
+
+orint=int(input('请输入一个数：'))
+
+
+
+def is_palindrome(num):
+    tempint=0
+    while num>0:
+        tempint=tempint*10+num%10
+        num//=10
+    print(tempint)
+    return(tempint==orint)
+    
+
+
+print(is_palindrome(orint))
+
+    tempint=num
+    for i in range(2,(tempint**0.5)+1):
+        count=1
+        if i %
+
+
+
+# 实现判断一个数是不是素数的函数
+is_pri=[]
+def is_prime(num):
+    for i in range(2,num+1):
+        for k in range(2,i+1):
+            if i % k==0:
+                if i == k:
+                    is_pri.append(i)
+                else:
+                    break
+    
+    
+
+is_prime(50)
+print(is_pri)
+                
+'''
